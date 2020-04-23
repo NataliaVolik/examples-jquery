9269fc4b430518ee982a24095390ef919bcfc000
NataliaVolik <log.litana@gmail.com>
2020-04-23 16:56:36 +0600

    add accordion

--- js/main.js
+++ js/main.js
@@ -100,5 +100,11 @@ submit.click(() => {
 closeModal.click(() => {
     modal.removeClass('show');
 })
-
+// Task 4
+$(document).ready(function () {
+    $('.panel-heading').click(function () {
+        $(this).toggleClass('in').next().slideToggle();
+        $('.panel-heading').not(this).removeClass('in').next().slideUp();
+    });
+});
 
